1d5b9764ebf58fdbde7357df208f3eeb352b354d
Evgeniy Baydukalov <baydukalov3@mail.ru>
2017-11-03 19:41:44 +0500

    input сделал более понятным

--- pprint_json.py
+++ pprint_json.py
@@ -2,13 +2,13 @@ import json
 
 
 def load_data(filepath):
-    with open(filepath, 'r') as file_handler:
-        return json.load(file_handler)
+    with open(filepath, 'r') as file:
+        return json.load(file)
 
 def pretty_print_json(data):
-    print(json.dumps(data, indent=4, ensure_ascii=False))
+    return json.dumps(data, indent=4, ensure_ascii=False, sort_keys=True)
 
 
 if __name__ == '__main__':
-    filepath = input(':   ')
+    filepath = input('enter the path to the file:   ')
     pretty_print_json(load_data(filepath))
