24ea16320ee4d6a024382b5349d38959d65d7a5a
Evgeniy Baydukalov <baydukalov3@mail.ru>
2017-11-03 19:51:21 +0500

    заменил название переменной

--- pprint_json.py
+++ pprint_json.py
@@ -5,8 +5,8 @@ def load_data(filepath):
     with open(filepath, 'r') as file:
         return json.load(file)
 
-def pretty_print_json(data):
-    return json.dumps(data, indent=4, ensure_ascii=False, sort_keys=True)
+def pretty_print_json(json_file):
+    return json.dumps(json_file, indent=4, ensure_ascii=False, sort_keys=True)
 
 
 if __name__ == '__main__':
