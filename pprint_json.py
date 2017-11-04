52e8d12ddf1f1367da6dfa99c417fe6e9a13a5a3
Evgeniy Baydukalov <baydukalov3@mail.ru>
2017-11-04 20:09:38 +0500

    поправил readme

--- pprint_json.py
+++ pprint_json.py
@@ -1,14 +1,16 @@
 import json
+import sys
 
 
 def load_data(filepath):
     with open(filepath, 'r') as file:
         return json.load(file)
 
+
 def pretty_print_json(json_file):
     return json.dumps(json_file, indent=4, ensure_ascii=False, sort_keys=True)
 
 
 if __name__ == '__main__':
-    filepath = input('enter the path to the file:   ')
+    filepath = sys.argv[1]
     pretty_print_json(load_data(filepath))
